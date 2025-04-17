e0df97542f478b006e7a96b4c05c03f3ecf33362
Sabeer <94005086+Syed-Sabeer@users.noreply.github.com>
2025-04-17 13:54:03 +0500

    cache added

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,19 @@ Route::get('/', function () {
   return redirect()->route('home');
 });
 
+Route::get('/sabeer', function() {
+  $exitCode = Artisan::call('cache:clear');
+  $exitCode = Artisan::call('config:clear');
+  $exitCode = Artisan::call('route:clear');
+  $exitCode = Artisan::call('view:clear');
+  $exitCode = Artisan::call('optimize');
+  $exitCode = Artisan::call('optimize:clear');
+  $exitCode = Artisan::call('route:cache');
+  
+
+  return "Cache is cleared"; // you can replce your redirect
+});
+
 // Route::prefix('main')->middleware(['web', 'CheckCountry'])->group(function () {
 Route::middleware(['web'])->group(function () {
 
